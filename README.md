d7903072f944662c175b5a0ae65962b361880c56
slothy <slothywasnottaken@gmail.com>
2025-12-28 21:24:51 -0600

    init of hearth

---
+++ README.md
@@ -0,0 +1,1 @@
+hearth is the name for my langauge that will be used to configure my home dir instead of nix if it goes well
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              libgit2
+            ];
+
+            buildInputs = with pkgs; [
+              pkg-config
+              libgit2
+            ];
+          };
+      }
+    );
+}
